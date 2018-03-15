a1771c5d8bda931f7dd4e153dd75a259647e00c4
Loviisa Hurme <Loviisa@eduroam-239-95.uta.fi>
2018-03-15 16:01:24 +0200

    More changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const CardWrapper = styled.div`
-	box-shadow: 3px black;
-	padding: 100 px;
-	max-width: 80 px;
+	box-shadow: 5px 10px 8px #888888;
+	padding: 2em;
+	width: 20em;
+	height: 30em;
 `;
 
 const Image = styled.img`
-	max-width: 70%;
+	max-width: 100%;
 `
 
 const Title = styled.h1`
-	color: red;
-	font-size: 2 em;
-	padding-bottom: 2 em;
+	
 `
 
 export default class Card extends React.Component {
